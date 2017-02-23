80eb469a42767930f0171347c696cc80f77bfe26
yoo <bell1346@gmail.com>
2017-02-23 23:52:11 +0900

    secret key

--- app/views.py
+++ app/views.py
@@ -3,12 +3,17 @@ from flask import Flask, session, url_for, escape
 from app import app
 import json, requests
 
+# set the secret key.  keep this really secret:
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+# app.secret_key = os.urandom(24)
+
+
 # index view function suppressed for brevity
 @app.route('/')
 @app.route('/index')
 def index():
-	if 'username' in session:
-		return 'Logged in as %s' % escape(session['username'])
+	if 'email' in session:
+		return 'Logged in as %s' % escape(session['email'])
 	else:
 		return render_template("search.html",
                         title='Welcome')
@@ -16,21 +21,24 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    error=None
-    if request.method == 'POST':
-        if request.form['email'] != app.config['EMAIL']:
-            error = 'Invalid username'
-        elif request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid password'
-        else:
-            # session['logged_in'] = True
-            # flash('You were logged in','normal')
+	error=None
+	if request.method == 'POST':
+		session['email'] = request.form['email']
+		return redirect('/index')
+	# if request.method == 'POST':
+	# 	if request.form['email'] != app.config['EMAIL']:
+	# 		error = 'Invalid username'
+	# 	elif request.form['password'] != app.config['PASSWORD']:
+	# 		error = 'Invalid password'
+	# 	else:
+	# 		# session['logged_in'] = True
+    #         # flash('You were logged in','normal')
+	# 		session['email'] = request.form['email']
+	# 		return redirect('/index')
 
-            return redirect('/main2')
-
-    return render_template("login.html",
-                        title='Sign In',
-                        error=error)
+	return render_template("login.html",
+						title='Sign In',
+						error=error)
 
 
 
